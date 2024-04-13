90cf13444876a20b0e7c1de7d87447b8dd070f20
AyeChanHtunNaing <ayechanhtunnaing@icloud.com>
2024-04-13 11:12:48 +0630

    feat : HTTPResponse

---
+++ usermanagement-api/src/main/java/dev/peacechan/usermanagement/domain/HttpResponse.java
@@ -0,0 +1,28 @@
+package dev.peacechan.usermanagement.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.util.Date;
+
+@Data
+public class HttpResponse {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy hh:mm:ss", timezone = "Asia/Yangon")
+    private Date timeStamp;
+    private int httpStatusCode; // 200, 201, 400, 500
+    private HttpStatus httpStatus;
+    private String reason;
+    private String message;
+
+    // Constructor never used. Can be (and should be) deleted
+    /*public HttpResponse() {}*/
+
+    public HttpResponse(int httpStatusCode, HttpStatus httpStatus, String reason, String message) {
+        this.timeStamp = new Date();
+        this.httpStatusCode = httpStatusCode;
+        this.httpStatus = httpStatus;
+        this.reason = reason;
+        this.message = message;
+    }
+}
