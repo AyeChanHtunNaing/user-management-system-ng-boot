5362e0c7cb5cc40784d0972858cc9b470346fd32
AyeChanHtunNaing <ayechanhtunnaing@icloud.com>
2024-04-14 21:30:58 +0630

    tAuthenticationEntryPoint

---
+++ usermanagement-api/src/main/java/dev/peacechan/usermanagement/filter/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,32 @@
+package dev.peacechan.usermanagement.filter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import dev.peacechan.usermanagement.constant.SecurityConstant;
+import dev.peacechan.usermanagement.domain.HttpResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Locale;
+
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Component
+public class JwtAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException arg2) throws IOException {
+        HttpResponse httpResponse = new HttpResponse(FORBIDDEN.value(), FORBIDDEN, FORBIDDEN.getReasonPhrase().toUpperCase(Locale.ROOT), SecurityConstant.FORBIDDEN_MESSAGE);
+        response.setContentType(APPLICATION_JSON_VALUE);
+        response.setStatus(FORBIDDEN.value());
+        OutputStream outputStream = response.getOutputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(outputStream, httpResponse);
+        outputStream.flush();
+    }
+}
