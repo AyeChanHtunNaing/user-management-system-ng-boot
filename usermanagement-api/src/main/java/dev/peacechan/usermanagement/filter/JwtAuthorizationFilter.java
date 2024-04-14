c6eb9dc8156069f22c2160537ee97c4bf5766efd
AyeChanHtunNaing <ayechanhtunnaing@icloud.com>
2024-04-14 21:04:53 +0630

    feat: JwtAuthorizationFilter

---
+++ usermanagement-api/src/main/java/dev/peacechan/usermanagement/filter/JwtAuthorizationFilter.java
@@ -0,0 +1,53 @@
+package dev.peacechan.usermanagement.filter;
+import dev.peacechan.usermanagement.constant.SecurityConstant;
+import dev.peacechan.usermanagement.utility.JwtTokenProvider;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthorizationFilter(JwtTokenProvider jwtTokenProvider){
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if(request.getMethod().equalsIgnoreCase(SecurityConstant.OPTIONS_HTTP_METHOD)){
+            response.setStatus(HttpStatus.OK.value());
+        }else{
+            String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+            if(authorizationHeader == null || !authorizationHeader.startsWith(SecurityConstant.TOKEN_PREFIX)){
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            String token = authorizationHeader.substring(SecurityConstant.TOKEN_PREFIX.length());
+            String username = this.jwtTokenProvider.getSubject(token);
+            if(jwtTokenProvider.isTokenValid(username, token) && SecurityContextHolder.getContext().getAuthentication() == null){
+                List<GrantedAuthority> authorities = jwtTokenProvider.getAuthorities(token);
+                Authentication authentication = jwtTokenProvider.getAuthentication(username, authorities, request);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }else{
+                SecurityContextHolder.clearContext();
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
--- usermanagement-api/src/main/java/dev/peacechan/usermanagement/utility/JwtTokenProvider.java
+++ usermanagement-api/src/main/java/dev/peacechan/usermanagement/utility/JwtTokenProvider.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -21,7 +22,7 @@ import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
 import static dev.peacechan.usermanagement.constant.SecurityConstant.*;
 import static java.util.Arrays.stream;
 
-
+@Component
 public class JwtTokenProvider {
     @Value("${jwt.secret}")
     private String secret;
