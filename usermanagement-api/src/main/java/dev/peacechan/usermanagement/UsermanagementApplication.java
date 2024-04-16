1e37e4a36c6fa6336e3d5203e24e52276d78a66e
AyeChanHtunNaing <ayechanhtunnaing@icloud.com>
2024-04-16 21:00:29 +0630

    feat: security config

--- usermanagement-api/src/main/java/dev/peacechan/usermanagement/UsermanagementApplication.java
+++ usermanagement-api/src/main/java/dev/peacechan/usermanagement/UsermanagementApplication.java
@@ -2,12 +2,18 @@ package dev.peacechan.usermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class UsermanagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsermanagementApplication.class, args);
+	}
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
--- usermanagement-api/src/main/java/dev/peacechan/usermanagement/configuration/SecurityConfiguration.java
+++ usermanagement-api/src/main/java/dev/peacechan/usermanagement/configuration/SecurityConfiguration.java
@@ -1,2 +1,64 @@
-package dev.peacechan.usermanagement.configuration;public class SecurityConfiguration {
+package dev.peacechan.usermanagement.configuration;
+
+
+import dev.peacechan.usermanagement.constant.SecurityConstant;
+import dev.peacechan.usermanagement.filter.JwtAccessDeniedHandler;
+import dev.peacechan.usermanagement.filter.JwtAuthenticationEntryPoint;
+import dev.peacechan.usermanagement.filter.JwtAuthorizationFilter;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private UserDetailsService userDetailsService;
+    private PasswordEncoder passwordEncoder;
+    private JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private JwtAuthorizationFilter jwtAuthorizationFilter;
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    public SecurityConfiguration(@Qualifier("userDetailsService") UserDetailsService userDetailsService, PasswordEncoder passwordEncoder, JwtAccessDeniedHandler jwtAccessDeniedHandler, JwtAuthorizationFilter jwtAuthorizationFilter, JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+        this.jwtAuthorizationFilter = jwtAuthorizationFilter;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(this.userDetailsService).passwordEncoder(this.passwordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests().antMatchers(SecurityConstant.PUBLIC_URLS).permitAll().anyRequest().authenticated()
+                .and()
+                .exceptionHandling().accessDeniedHandler(this.jwtAccessDeniedHandler).authenticationEntryPoint(this.jwtAuthenticationEntryPoint)
+                .and()
+                .addFilterBefore(this.jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 }
